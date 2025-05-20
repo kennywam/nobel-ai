a40e648f56fe39b6f7d31f3a531b201169289f1a
Kennedy Wambua <kennywambu254@gmail.com>
2025-05-21 00:54:20 +0300

    chore: update app module imports config

--- packages/backend-api/src/app.module.ts
+++ packages/backend-api/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OpenAiModule } from './core/open-ai/open-ai.module';
 
 @Module({
-  imports: [OpenAiModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    OpenAiModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
